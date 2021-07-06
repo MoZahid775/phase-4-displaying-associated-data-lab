2a6876fe5643fc196055d660e9f21e8ca3360cc6
Muhammad Zahid <muhammadali775@gmail.com>
2021-07-06 01:31:56 -0400

    completed assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+    
+    
+    
+    
+    def show
+
+
+      user= User.find(params[:id])
+      render json: user, include: :items
+
+    end
 end
